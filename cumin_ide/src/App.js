5a640bfb23c677bc4abc744b048b801148648ca0
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2020-08-31 18:19:20 -0400

    Added CodeMirror online editor

--- cumin_ide/src/App.js
+++ cumin_ide/src/App.js
@@ -1,27 +1,33 @@
-import CodeMirror from '@uiw/react-codemirror';
-import 'codemirror/keymap/sublime';
-import 'codemirror/theme/monokai.css';
-import React, {Component} from 'react';
-import Docs1 from './components/Todos';
-import "./App.css"
-const code = 'const a = 0;';
-class App extends Component{
-  render (){
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import React, { Component } from "react";
+import NavBar from "./components/Navbar/index";
+import {cuminDetector} from "./components/ide/cuminDetector"
+import "./App.css";
+import CodeMirrorLib from 'codemirror';
+CodeMirrorLib.defineMode('yourMode', cuminDetector);
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/javascript/javascript');
+class App extends Component {
+  render() {
     return (
-      <div className="App">
-         <CodeMirror
-        value={code}
-        options={{
-        theme: 'monokai',
-        keyMap: 'sublime',
-        mode: 'jsx',
-      }}
-    />
-        <Docs1 />
+      <div className="wrapper">
+        <NavBar></NavBar>
+        <div className="ide">
+          < CodeMirror className = "CodeMirror"
+            options={{
+              defineMode: {name: "cumin", fn: cuminDetector()},
+              theme: "material",
+              lineNumbers: true,
+            }}
+            onChange={(editor, data, value) => {
+              console.log(value);
+            }}
+          />
+          <div className="docs"></div>
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
---
+++ cumin_ide/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-light bg-light">
+                    <span className="navbar-brand mb-0 h1">Cumin Online IDE</span>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default NavBar
