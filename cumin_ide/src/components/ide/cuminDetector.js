ae439c6c9d62f9706b4882da4297be65f6a1fb7d
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2020-09-02 02:29:48 -0400

    detecting strings

--- cumin_ide/src/components/ide/cuminDetector.js
+++ cumin_ide/src/components/ide/cuminDetector.js
@@ -36,20 +36,19 @@ export function cuminDetector() {
       }
 
 
-    //   if (state.inString) {
-    //     if (stream.skipTo('"')) {
-    //       // Quote found on this line
-    //       stream.next(); // Skip quote
-    //       state.inString = false; // Clear flag
-    //     } else {
-    //       stream.skipToEnd(); // Rest of line is string
-    //     }
-    //     return "string"; // Token style
-    //   } else {
-    //     stream.skipTo('"') || stream.skipToEnd();
-    //     return null; // Unstyled token
-    //   }
-      stream.next();
+      if (state.inString) {
+        if (stream.skipTo('"')) {
+          // Quote found on this line
+          stream.next(); // Skip quote
+          state.inString = false; // Clear flag
+        } else {
+          stream.skipToEnd(); // Rest of line is string
+        }
+        return "string"; // Token style
+      } else {
+        stream.skipTo('"') || stream.next();
+      }
+      
     },
   };
 }
