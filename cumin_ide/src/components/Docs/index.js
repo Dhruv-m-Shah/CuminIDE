a7c9e45eb5704d55b5205e53a8038a4910582c42
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2020-09-10 00:27:42 -0400

    Added in code snippets and examples

--- cumin_ide/src/components/Docs/index.js
+++ cumin_ide/src/components/Docs/index.js
@@ -1,17 +1,49 @@
-import React, { Component } from 'react'
-import "../Docs/style.css"
-
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import React, { Component } from "react";
+import "../Docs/style.css";
+import CodeMirrorLib from "codemirror";
+import { cuminDetector } from "../ide/cuminDetector";
+CodeMirrorLib.defineMode("yourMode", cuminDetector);
+require("codemirror/mode/xml/xml");
+require("codemirror/mode/javascript/javascript");
 export class Docs extends Component {
-    render() {
-        return (
-            <div className = "Content_wrapper"> 
-                <h2>Cumin Documentation</h2>
-                <h3>What is Cumin?</h3>
-                <p>Cumin is an interpreted programming language made in C++. The lexer, parser, and interpreter were made from scratch, check out the code <a target="_blank" href = "https://github.com/Dhruv-m-Shah/Cumin">here!</a></p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Content_wrapper">
+        <h2>Cumin Documentation</h2>
+        <h5>What is Cumin?</h5>
+        <p>
+          Cumin is an interpreted programming language made in C++. The lexer,
+          parser, and interpreter were made from scratch, check out the code{" "}
+          <a target="_blank" href="https://github.com/Dhruv-m-Shah/Cumin">
+            here!
+          </a>
+        </p>
+        <h5>Basics of Cumin</h5>
+        <p>
+          The main entry point for a cumin application is indicated by the{" "}
+          <code className = "qualifier">start</code> keyword and the end is indicating with the{" "}
+          <code className = "qualifier">end</code> keyword. It should look something like this:
+        </p>
+        <CodeMirror
+          className="CodeMirror"
+          value =" start 
+          // ENTER CODE HERE
+        end"
+          options={{
+            defineMode: { name: "cumin", fn: cuminDetector() },
+            theme: "material",
+            lineNumbers: true,
+            readOnly: "nocursor"
+          }}
+          onChange={(editor, data, value) => {
+            this.setState({ code: value });
+            console.log(this.state.code);
+          }}
+        />
+      </div>
+    );
+  }
 }
 
-
-export default Docs
+export default Docs;
